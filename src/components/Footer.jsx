97d19f96a9cd2b5c53fd9e2cbc6047ec26ce26d2
Aryan Gajjar <gajjararyan1509@gmail.com>
2024-05-09 10:32:39 +0530

    new commmit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,9 @@ const Footer = () => (
           alt="hoobank"
           className="w-[266px] h-[72.14px] object-contain"
         />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make the payments easy, reliable and secure.
+        <p className={`${styles.paragraph} mt-4 max-w-[347px]`}>
+        Empowering People and Businesses to Thrive in the IT World 
+        Pioneering Excellence in IT Consulting & Services.
         </p>
       </div>
 
@@ -40,8 +41,8 @@ const Footer = () => (
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+      <p className="font-poppins font-normal text-center text-[17px] leading-[27px] text-white">
+        Made with by 🩵 Stark Technologies
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
