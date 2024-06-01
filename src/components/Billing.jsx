6fc177817025ed2415865b09a0d90604a8d27aeb
Aryan Gajjar <gajjararyan1509@gmail.com>
2024-06-01 21:26:37 +0530

    done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,10 +1,10 @@
-import { apple, bill, google } from "../assets";
+import { apple, google} from "../assets";
 import styles, { layout } from "../style";
 
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
-      <img src="../assets/think.png" alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <img src="../assets/robot-ai.png" alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
       {/* gradient start */}
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
